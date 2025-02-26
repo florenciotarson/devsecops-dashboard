6d490f87b5a6dc4563a11e365df2d1fe83eebe4d
florencio <florencio.tarson@icloud.com>
2025-02-26 00:42:57 -0300

    Remove frontend submodule from index

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export const fetchSecureData = async (accessToken) => {
-    const response = await fetch("http://localhost:5000/secure-data", {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    return response.json();
-};
